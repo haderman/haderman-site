291b11aa79d8fa9c1897da272890719f3c692137
Hader Cardona <hadermania@gmail.com>
2021-03-31 14:21:47 -0500

    Update cv

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -50,17 +50,16 @@ export default function Cv({ data }) {
           </div>
         </div>
         <article>
-          <h1>PROFILE</h1>
+          <h1>Who I am</h1>
           <p>
             I’m a Front End Developer and I am very adaptive to different ways of working.
             I believe that the most important thing is to learn the basics very well and
-            determine the best approach for the team and the product overall. This is why
-            in my side projects I try to use fewer libraries as possible and understand
-            how things work fundamentally. I really love what I do and am continuously
-            learning and developing my skills. One of my professional goals is to have a personal
-            product in Product Hunt.
+            determine the best approach for the team and the product overall. I love to have
+            side projects to experiment with new technologies and to practice new things that
+            I learn on my way. I really love what I do and I am continuously
+            learning and developing my skills.
           </p>
-          <h1>SKILLS</h1>
+          <h1>Skills</h1>
           <ul className="skill-list">
             <li>React</li>
             <li>Html</li>
@@ -68,11 +67,15 @@ export default function Cv({ data }) {
             <li>Redux</li>
             <li>Nextjs</li>
             <li>Gatsbyjs</li>
+            <li>Storybook</li>
+            <li>Github actions</li>
+            <li>Electron</li>
+            <li>Typescript</li>
           </ul>
         </article>
       </section>
       <section className="cv-side-projects-container">
-        <h1>SIDE PROJECTS</h1>
+        <h1>Side Projects</h1>
         <ul className="cv-side-projects-list">
           <li className="cv-side-project-item cv-border-top-blue">
             <h2>Personal Site</h2>
@@ -90,11 +93,11 @@ export default function Cv({ data }) {
             </p>
             <ul className="cv-side-project-links">
               <li>
-                <GithubIcon />
+                <GithubIcon  width={20} />
                 <span>https://github.com/haderman/haderman-site</span>
               </li>
               <li>
-                <WorldIcon />
+                <WorldIcon  width={20} />
                 <span>https://haderman.netlify.app</span>
               </li>
             </ul>
@@ -115,11 +118,11 @@ export default function Cv({ data }) {
             </p>
             <ul className="cv-side-project-links">
               <li>
-                <GithubIcon />
+                <GithubIcon width={20} />
                 <span>https://github.com/haderman/nextjs-covid-19</span>
               </li>
               <li>
-                <WorldIcon />
+                <WorldIcon  width={20} />
                 <span>https://covidx19.vercel.app</span>
               </li>
             </ul>
@@ -139,7 +142,7 @@ export default function Cv({ data }) {
             </p>
             <ul className="cv-side-project-links">
               <li>
-                <GithubIcon />
+                <GithubIcon width={20} />
                 <span>https://github.com/haderman/woki-extension</span>
               </li>
             </ul>
@@ -147,9 +150,8 @@ export default function Cv({ data }) {
         </ul>
       </section>
       <section className="cv-experience">
-        <h1>EXPERIENCE</h1>
+        <h1>Experience</h1>
         <div className="cv-timeline">
-          {/* <div className="cv-timeline-line" /> */}
           <ul>
             <li>
               <article className="cv-experience-item">
@@ -157,9 +159,11 @@ export default function Cv({ data }) {
                 <p>
                   Unity Developer Junior at <strong>Brainz.</strong> (Medellín, Colombia)
                 </p>
-                <p>
-                  I helped to develop several components of the ui of the game including the store, modals and rewards
-                </p>
+                <ul>
+                  <li>
+                    I helped to develop several components of the ui of the game including the store, modals and rewards
+                  </li>
+                </ul>
               </article>
             </li>
             <li>
@@ -168,9 +172,11 @@ export default function Cv({ data }) {
                 <p>
                   Front End Developer at <strong>Equitel.</strong> (Medellín, Colombia)
                 </p>
-                <p>
-                  I built a module for Artimo (a platform of the company) using React and Redux
-                </p>
+                <ul>
+                  <li>
+                    I built a module for Artimo (a platform of the company) using React and Redux
+                  </li>
+                </ul>
               </article>
             </li>
             <li>
@@ -189,19 +195,29 @@ export default function Cv({ data }) {
             <li>
               <article className="cv-experience-item">
                 <h4>2019</h4>
-                <p>
-                  I travelled and worked as a volunteer in different hostels in Ecuador and Peru to learn English
-                </p>
+                <ul>
+                  <li>
+                    I was traveling as a backpacker and working as a volunteer in different places to practice English 
+                  </li>
+                </ul>
+              </article>
+            </li>
+            <li>
+              <article className="cv-experience-item">
+                <h4>2021</h4>
+                <p>Front End Developer at <strong>Somoglobal</strong> (present)</p>
+                <ul>
+                  <li>
+                    Create components for mettrr project using React and Nextjs
+                  </li>
+                  <li>
+                    Update Somoglobal website to the new design using React and Gatsby
+                  </li>
+                </ul>
               </article>
             </li>
           </ul>
         </div>
-      </section>
-      <section className="cv-education">
-        <h1>EDUCATION</h1>
-        <p>
-          Institución Universitaria de Envigado, Systems Engineer JAN 2008 - DEC 2014, MEDELLÍN (Colombia)
-        </p>
       </section>
     </div>
   );
